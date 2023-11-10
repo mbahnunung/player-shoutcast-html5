bb99ce8368edd35e28801a4a564574c863828f00
mbahnunung <mbahnunung@users.noreply.github.com>
2023-11-11 01:44:39 +0700

    Update kano.js

--- js/kano.js
+++ js/kano.js
@@ -1,7 +1,7 @@
 const RADIO_NAME = 'mbah nunung Online Radio';
 
 // SELECT ARTWORK PROVIDER, ITUNES, DEEZER & SPOTIFY  eg : spotify 
-var API_SERVICE = 'ITUNES';
+var API_SERVICE = 'spotify';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
 const URL_STREAMING = 'https://stream.zeno.fm/n4gzbe9ufzzuv';
