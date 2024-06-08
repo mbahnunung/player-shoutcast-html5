717a961fc5d20b8b1c9f093d4bc300d0f6ca1f68
mbahnunung <mbahnunung@users.noreply.github.com>
2024-06-09 02:05:13 +0700

    Update combro.js

--- js/combro.js
+++ js/combro.js
@@ -1098,7 +1098,7 @@ function getStreamingData() {
     var d = new Date();
 
     // Requisition with timestamp to prevent cache on mobile devices
-    xhttp.open('GET', 'https://api.streamafrica.net/zeno/index.php?z='+zenoid);
+    xhttp.open('GET', API_URL);
     xhttp.send();
 }
 
