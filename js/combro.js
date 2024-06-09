7fadd3f507c6b4365207fba3c7986decafc95f7b
mbahnunung <mbahnunung@users.noreply.github.com>
2024-06-09 19:42:07 +0700

    Update combro.js

--- js/combro.js
+++ js/combro.js
@@ -104,7 +104,7 @@ function Page() {
         const Rozy = 'https://yt3.googleusercontent.com/k3FluG3Cks7LH-1dveRTySBs2p7dUgvdDbV_Cd5wE7njxyyiGBL9brJ7xGHaZ5GY6ocrVv-9dg=s900-c-k-c0x00ffffff-no-rj';
         const Reny = 'https://viberatecdn.blob.core.windows.net/entity/artist/reny-farida-sWRmh';
         const Virgia_Hassan = 'https://i.ytimg.com/vi/g3A7Cp2yAro/maxresdefault.jpg';
-        const MAJAO = 'img/izveduvaci/MAJAO.jpg';
+        const Kurnia_Dewi = 'https://resources.tidal.com/images/bd1705a5/abba/49dc/ab94/b249c523b790/640x640.jpg';
         const MAGIJA = 'img/izveduvaci/MAGIJA.jpg';
         const TOSE = 'img/izveduvaci/TOSE.jpg';
         const MARIJAN = 'img/izveduvaci/MARIJAN.jpg';
@@ -255,8 +255,8 @@ function Page() {
         else if (artistRadio = 'BTS'){
             var urlCoverArt = BTS;
         }
-        else if (artistRadio == 'MAGDALENA CVETKOSKA'){
-            var urlCoverArt = MAGDALENAC;
+        else if (artistRadio == 'Kurnia Dewi'){
+            var urlCoverArt = Kurnia_Dewi;
         }
         else if (artistRadio == 'IGOR DZAMBAZOV'){
             var urlCoverArt = IGOR;
